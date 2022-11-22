2c88958ad7e47b870e3024279d398749ee365355
Jesse Box <me@jessebox.net>
2022-11-22 10:21:13 +0100

    [Added] config-overrides.js

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+// config-overrides.js
+module.exports = function override(config, env) {
+	// New config, e.g. config.plugins.push...
+	config.devtool = 'source-map';
+	config.plugins.push(
+		new SentryWebpackPlugin({
+			// sentry-cli configuration - can also be done directly through sentry-cli
+			// see https://docs.sentry.io/product/cli/configuration/ for details
+			org: 'jesse-box',
+			project: 'javascript-react',
+			include: './dist',
+			authToken: process.env.SENTRY_AUTH_TOKEN,
+		})
+	);
+	return config;
+};
